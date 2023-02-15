c31aa869ca30ef11321df806149f19ebcd2ec73f
Godfrey-Vila <vila.godfreyc@gmail.com>
2023-02-15 20:30:37 +0800

    making an if statement for the right and left data in order

--- Binary_Tree_Phonebook.py
+++ Binary_Tree_Phonebook.py
@@ -38,3 +38,12 @@ class BinarySearchTreeNode:
 
     def in_order_traversal(self):
         elements = []
+
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        elements.append(self.data)
+
+        if self.right:
+            elements += self.right.in_order_traversal()
+        return elements
