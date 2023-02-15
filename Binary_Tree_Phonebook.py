01628d401ae4c42dd4a795975bc71e9ba7d5c554
Godfrey-Vila <vila.godfreyc@gmail.com>
2023-02-15 20:19:47 +0800

    creating def mnamed menu that shows the options of the user

--- Binary_Tree_Phonebook.py
+++ Binary_Tree_Phonebook.py
@@ -4,3 +4,14 @@ print("******** SECTION BSCOE 2-2 ***********")
 print("******* Sir Danilo Madrigalejos ******")
 print("")
 my_name = []
+def menu():
+    print("")
+    print("************PHONE BOOK*************")
+    print("")
+
+    print(" What do you like to do?: ")
+    print("1. Add Contact")
+    print("2. Delete Contact")
+    print("3. View Contact")
+    print("4. Exit")
+
